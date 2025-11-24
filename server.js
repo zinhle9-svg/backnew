0f07fa4da16746e827247ca4fc1a2a2e77298743
zinhle <ngwanejeanette3@gmail.com>
2025-11-24 23:35:38 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+ const app = express();
+const cors = require('cors');
+const products = require('./products').default;
+   
+app.use(cors());
+app.use(express.json());
+
+    // Get all products
+    app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+//get each product
+    app.get("/api/products/:id", (req, res) => {
+    const product = products.find((item) => item.id === id);
+ // const id = products.id; why not this?
+  const id= parseInt(req.params.id);
+   if (!product) {
+  return res.status(404).json({ message: "Product not found" });
+}
+ res.json(product);
+});
+
+
+    const port = process.env.PORT || 4000;
+    app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    });
+    
---
+++ users.js
@@ -0,0 +1,4 @@
+ let users = [
+    { id: 1, name: 'John Doe', email: 'john@example.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane@example.com' }
+    ];
